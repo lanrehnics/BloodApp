c7cd97b36753f99d092a097582266809491fc251
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-07-06 14:48:45 +1000

    Remove about page and add map page

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/map/map.html'
 })
-export class AboutPage {
+export class MapPage {
   constructor(private navController: NavController) {
   }
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core'
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
+import {MapPage} from '../map/map';
 import {DatePage} from '../date/date';
 
 @Component({
@@ -16,7 +16,7 @@ export class TabsPage {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
+    this.tab2Root = MapPage;
     this.tab3Root = DatePage;
   }
 }
