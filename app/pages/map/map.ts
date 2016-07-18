dce52f14831440305587d03352c06090adea4bd7
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-07-18 22:41:02 +1000

    Add Google Places Search to MapPage

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -6,10 +6,14 @@ import {Geolocation} from 'ionic-native';
   templateUrl: 'build/pages/map/map.html'
 })
 export class MapPage {
-  map;
-  userLatLng;
+  map: any;
+  userLatLng: any;
+  service: any;
   constructor(private navController: NavController) {
     this.saveLocation();
+  }
+  ngOnInit(){
+    this.loadMap();
   }
   loadMap(){
     this.saveLocation();
@@ -20,39 +24,43 @@ export class MapPage {
     }
     this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
-saveLocation(){
-  navigator.geolocation.getCurrentPosition(
-      (position) => {
-          this.userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      },
-      (error) => {
+  saveLocation(){
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            this.userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        },
+        (error) => {
           console.log(error);
-      }
-  );
+        }
+    );
+  }
+  addInfoWindow(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', function(){
+      infoWindow.open(this.map, marker);
+    });
+  }
+  searchPlaces(){
+    this.service = new google.maps.places.PlacesService(this.map);
+    this.service.textSearch({
+          location: this.userLatLng,
+          radius: 3000,
+          type: ['point_of_interest'],
+          keyword: ['Australian Red Cross Blood Service']
+        }, (results) => {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            var map = this.map;
+            var placeLoc = results[i].geometry.location;
+            var marker = new google.maps.Marker({
+              map: this.map,
+              position: results[i].geometry.location
+            });
+            this.addInfoWindow(marker, place.name)
+          };
+        } );
+
 }
-addMarker(){
-
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
-
-  let content = "<h4>Information!</h4>";
-
-  this.addInfoWindow(marker, content);
-
-}
-addInfoWindow(marker, content){
-
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
-
-  google.maps.event.addListener(marker, 'click', function(){
-    infoWindow.open(this.map, marker);
-  });
-
-}
-
 }
