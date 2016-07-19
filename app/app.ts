d7c4718288e7ae83805274b339e1f6a9c16da2ec
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-07-19 21:11:50 +1000

    Change Splash Screen

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
-
+import {Splashscreen} from 'ionic-native';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -19,6 +19,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       StatusBar.backgroundColorByHexString('#d32f2f');
+      Splashscreen.hide();
     });
   }
 }
