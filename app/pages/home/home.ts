f25befe2b7a62b07dcba32cd6f00e47843cf6b1e
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-07-21 20:44:35 +1000

    Add Firebase Authentication

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,18 +1,37 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {QuizPage} from '../quiz/quiz';
+import {LoginPage} from '../login/login';
+import {SignupPage} from '../signup/signup';
+import {AngularFire, FirebaseListObservable, FirebaseAuth, AngularFireAuth, AuthProviders} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+
 })
 export class HomePage {
-
-  constructor(private navController: NavController, private params: NavParams) {
+  items: FirebaseListObservable<any[]>;
+  authyState: any;
+  constructor(private navController: NavController, private params: NavParams, public af: AngularFire) {
     this.navController = navController;
+
+  }
+  ngOnInit(){
+    this.getFData();
+  }
+  getFData(){
+    this.items = this.af.database.list('/'  );
   }
   quizPush() {
     this.navController.push(QuizPage, {
             id : 0
         });
+  }
+  LoginPush(){
+    this.navController.push(LoginPage);
+  }
+  SignupPush(){
+    this.navController.push(SignupPage);
   }
 }
---
+++ app/pages/login/login.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, ViewController} from 'ionic-angular';
+import {AngularFire, firebaseAuthConfig, AuthProviders, AuthMethods, FirebaseAuth} from 'angularfire2';
+@Component({
+  templateUrl: 'build/pages/login/login.html',
+})
+export class LoginPage {
+  error: any;
+  constructor(private nav: NavController, public auth: FirebaseAuth, public viewCtrl: ViewController) {
+
+  }
+  dismiss() {
+        this.viewCtrl.dismiss();
+    }
+  login(credentials) {
+       this.auth.login(credentials).then((value) => {
+           console.log(value)
+           this.dismiss()
+       }).catch((error) => {
+           this.error = error
+           console.log(error)
+       });
+   }
+}
