8a108c0ea3ed243c160df8c1dad5e46bc9de794d
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-09-13 10:25:25 +1000

    Changes for web version

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import {Platform, Storage, LocalStorage} from 'ionic-angular';
 import {HomePage} from '../home/home';
 import {MapPage} from '../map/map';
 import {DatePage} from '../date/date';
-import {NativeMapPage} from '../native-map/native-map';
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
@@ -16,18 +15,8 @@ export class TabsPage {
   constructor(private platform: Platform) {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.storage.get('mapMode').then((success)=>{
-      if (success == "native") {
-        this.tab2Root = NativeMapPage;
-      }
-      else {
-        this.tab2Root = MapPage;
-      }
-    }).catch((error)=> {
-      this.tab2Root = MapPage;
-    });
+    this.tab2Root = MapPage;
     this.tab1Root = HomePage;
-
     this.tab3Root = DatePage;
   }
 }
