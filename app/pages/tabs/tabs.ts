a71cb0ea08c515764a02002f624f447aa244301a
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-09-21 12:02:23 +1000

    Remove JS map

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {Platform, Storage, LocalStorage} from 'ionic-angular';
 import {HomePage} from '../home/home';
-import {MapPage} from '../map/map';
 import {DatePage} from '../date/date';
 import {NativeMapPage} from '../native-map/native-map';
 @Component({
@@ -16,18 +15,8 @@ export class TabsPage {
   constructor(private platform: Platform) {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.storage.get('mapMode').then((success)=>{
-      if (success == "native") {
-        this.tab2Root = NativeMapPage;
-      }
-      else {
-        this.tab2Root = MapPage;
-      }
-    }).catch((error)=> {
-      this.tab2Root = MapPage;
-    });
     this.tab1Root = HomePage;
-
+    this.tab2Root = NativeMapPage;
     this.tab3Root = DatePage;
   }
 }
