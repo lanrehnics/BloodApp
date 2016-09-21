d275f75646a77f7577fa872893015c4abf60b0f6
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-09-21 18:22:55 +1000

    Remove Firebase

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,33 +1,14 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {QuizPage} from '../quiz/quiz';
-import {LoginPage} from '../login/login';
-import {SettingsPage} from '../settings/settings';
-import {AngularFire, FirebaseListObservable, FirebaseAuth, AngularFireAuth, AuthProviders} from 'angularfire2';
-import {Observable} from 'rxjs/Observable';
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
 
 })
 export class HomePage {
-  items: FirebaseListObservable<any[]>;
-  constructor(private navController: NavController, private params: NavParams, public af: AngularFire) {
+  constructor(private navController: NavController, private params: NavParams) {
     this.navController = navController;
-    firebase.auth().onAuthStateChanged((user)=> {
-      if (user) {
-        this.items = this.af.database.list('/users/' + user.uid);
-      }
-      else {
-        this.items = null;
-      }
-    });
-  }
-  loginPush(){
-    this.navController.push(LoginPage);
-  }
-  settingsPush(){
-    this.navController.push(SettingsPage);
   }
   quizPush() {
     this.navController.push(QuizPage, {
